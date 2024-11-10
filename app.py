ce911474429ee685f6f2b9ac03fd0c4c2150feb8
Cliff Xuan <cliff.xuan@gmail.com>
2024-11-10 16:32:25 +0000

    Refactor app.py: - Moved import of Path for clarity. - Updated stock data retrieval to include security names. - Changed history duration to 10 years. - Modified plot title to "Close Price". - Adjusted initial stock selection in dropdown.

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
+from pathlib import Path
+
 import gradio as gr
 import pandas as pd
 import plotly.graph_objects as go
 import yfinance as yf
-from pathlib import Path
-
-columns = ["Date", "Open", "High", "Low", "Close", "Volume"]
 
 PWD = Path(__file__).absolute().parent
-
-stocks = list(pd.read_csv(PWD / "data" / "spx-500.csv")["Symbol"])
+stock_df = pd.read_csv(PWD / "data" / "spx-500.csv")
+stocks = [
+    (f"{record['Security']} ({record['Symbol']})", record["Symbol"])
+    for record in stock_df.to_dict("records")
+]
+columns = ["Date", "Open", "High", "Low", "Close", "Volume"]
 
 
 def get_stock_data(ticker: str) -> tuple[pd.DataFrame | None, go.Figure]:
-    if not ticker:
-        return None
-    df = yf.Ticker(ticker).history().reset_index()
+    df = yf.Ticker(ticker).history("10y").reset_index()
     df["Date"] = df["Date"].dt.strftime("%Y-%m-%d")
     fig = go.Figure()
     fig.add_trace(
@@ -28,7 +29,7 @@ def get_stock_data(ticker: str) -> tuple[pd.DataFrame | None, go.Figure]:
     )
 
     fig.update_layout(
-        title=f"{ticker} Stock Price",
+        title=f"{ticker} Close Price",
         xaxis_title="Date",
         yaxis_title="Price (USD)",
         hovermode="x unified",
@@ -36,13 +37,17 @@ def get_stock_data(ticker: str) -> tuple[pd.DataFrame | None, go.Figure]:
         height=400,
     )
 
-    return df[columns], fig
+    return df[columns], fig  # type: ignore
 
 
-initials = get_stock_data(stocks[0])
+initials = get_stock_data(stocks[0][1])
 with gr.Blocks() as demo:
     gr.Markdown("## Stock Price")
-    ticker = gr.Dropdown(choices=stocks, value=stocks[0], label="Ticker")
+    ticker = gr.Dropdown(
+        choices=stocks,
+        value=stocks[0][1],
+        label="Ticker",
+    )
     stock_table = gr.DataFrame(
         value=initials[0],
         headers=columns,
